17fba6a8bad4c86e3c00189ace8910fc5c8b6c8b
SeungYongShim <syshim@me.com>
2023-04-16 13:15:32 +0900

    .

--- apps/poc-1/src/cats/cats.controller.spec.ts
+++ apps/poc-1/src/cats/cats.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CatsController } from './cats.controller';
+import { CatsService } from '@app/cats/cats.service';
 
 describe('CatsController', () => {
   let controller: CatsController;
@@ -7,12 +8,15 @@ describe('CatsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CatsController],
+      providers: [CatsService],
     }).compile();
 
     controller = module.get<CatsController>(CatsController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(controller.getCats()).toEqual({
+      message: 'Cats!'
+    })
   });
 });
